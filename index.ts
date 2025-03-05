fbbfb02b3c5c6ab39e75680b2641571a1b034418
jkinyanjui <joseph.kinyanjui@codecentric.de>
2025-03-05 16:23:24 +0100

    add object to bucket

--- index.ts
+++ index.ts
@@ -5,5 +5,10 @@ import * as awsx from "@pulumi/awsx";
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.BucketV2("pulumi-intro-bucket", {tags: {name: "pulumi-tutorial"}});
 
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    bucket: bucket.id,
+    source: new pulumi.asset.FileAsset("website/index.html")
+});
+
 // Export the name of the bucket
 export const bucketName = bucket.id;
