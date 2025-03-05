d7151c079b6def82ad3d0845c41f26399bd65f40
jkinyanjui <joseph.kinyanjui@codecentric.de>
2025-03-05 16:34:42 +0100

    make static website publicly available

--- index.ts
+++ index.ts
@@ -1,14 +1,35 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.BucketV2("pulumi-intro-bucket", {tags: {name: "pulumi-tutorial"}});
 
-const bucketObject = new aws.s3.BucketObject("index.html", {
+const website = new aws.s3.BucketWebsiteConfigurationV2("website", {
     bucket: bucket.id,
-    source: new pulumi.asset.FileAsset("website/index.html")
+    indexDocument: {
+        suffix: "index.html"
+    }
 });
 
-// Export the name of the bucket
+const ownershipControl = new aws.s3.BucketOwnershipControls("ownership-ctrl", {
+    bucket: bucket.id,
+    rule: {
+        objectOwnership: "ObjectWriter"
+    }
+})
+
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
+    bucket: bucket.id,
+    blockPublicAcls: false,
+});
+
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    bucket: bucket.id,
+    source: new pulumi.asset.FileAsset("website/index.html"),
+    contentType: "text/html",
+    acl: "public-read",
+}, {dependsOn: [publicAccessBlock, ownershipControl, website]});
+
+// Export the name of the bucket and endpoint
 export const bucketName = bucket.id;
+export const bucketEndpoint = pulumi.interpolate`http://${website.websiteEndpoint}`;
